19c1008ed778fd1ea88b05e54bb31add38c77370
Manan Joshi <mananjoshi1995@gmail.com>
2020-02-07 20:10:13 -0500

    Adding sytles for the images container in the side section.

--- src/views/RecipePreview/index.js
+++ src/views/RecipePreview/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Wrapper, SideSection, MainSection } from './styled';
+import {
+  Wrapper,
+  SideSection,
+  MainSection,
+  Content,
+  Image,
+  ImagesContainer
+} from './styled';
 import CardContainer from '../../components/Card/styled';
 
 function RecipePreview() {
@@ -11,8 +18,28 @@ function RecipePreview() {
           width='calc(100% - 20px)'
           height='380px'
           margin-top='50px'
-          margin-right='20px'
-        />
+          margin-right='20px'>
+          <Content>
+            <Image
+              height='150px'
+              width='100%'
+              background-color='red'
+              margin-bottom='10px'
+            />
+            <ImagesContainer>
+              {new Array(21).fill(0).map(() => (
+                <Image
+                  display='inline-block'
+                  height='80px'
+                  width='calc(33% - 6.5px)'
+                  margin-bottom='10px'
+                  margin-right='10px'
+                  adjust
+                />
+              ))}
+            </ImagesContainer>
+          </Content>
+        </CardContainer>
       </SideSection>
       <MainSection>
         <CardContainer
--- src/views/RecipePreview/styled.js
+++ src/views/RecipePreview/styled.js
@@ -20,4 +20,39 @@ const MainSection = styled.section`
   overflow: hidden;
 `;
 
-export { Wrapper, SideSection, MainSection };
+const Content = styled.div`
+  margin: 25px;
+  height: calc(100% - 50px);
+  width: calc(100% - 50px);
+`;
+
+const ImagesContainer = styled.div`
+  height: calc(100% - 160px);
+  width: 100%;
+  overflow-y: scroll;
+`;
+
+const Image = styled.div`
+  height: ${props => props.height && props.height};
+  width: ${props => props.width && props.width};
+  background-image: ${props => props.image && props.image};
+  background-position: center center;
+  background-repeat: no-repeat;
+
+  ${({ adjust }) =>
+    adjust &&
+    `
+    :nth-child(3n) {
+      margin-right: 0;
+    }
+    :nth-last-child(-n+3) {
+      margin-bottom: 0;
+    }
+  `}
+
+  ${({ children, height, width, ...rest }) => {
+    return { ...rest };
+  }}
+`;
+
+export { Wrapper, SideSection, MainSection, Content, ImagesContainer, Image };
