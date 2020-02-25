0861c9ba84a39e984236ab247cf8348b66cafaac
Manan Joshi <mananjoshi1995@gmail.com>
2020-02-25 00:16:52 -0500

    Using process env for api url

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,5 +1,9 @@
 // dev env
 const PROTOCOL = 'http';
-const URL = 'localhost:1741';
+const URL = `${
+  process.env.NODE_ENV !== 'development'
+    ? 'sculptor.mananjoshi.me'
+    : 'localhost'
+}:1741`;
 
 export { PROTOCOL, URL };
