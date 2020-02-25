5a9a7ce2a7d21dd7781df3fe0e7470d3e6d59082
Manan Joshi <mananjoshi1995@gmail.com>
2020-02-25 11:12:19 -0500

    localhost URL doesn't work. Resetting to process env

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,10 +1,9 @@
 // dev env
 const PROTOCOL = 'http';
-// const URL = `${
-//   process.env.NODE_ENV !== 'development'
-//     ? 'sculptor.mananjoshi.me'
-//     : 'localhost'
-// }:1741`;
-const URL = 'localhost:1741';
+const URL = `${
+  process.env.NODE_ENV !== 'development'
+    ? 'sculptor.mananjoshi.me'
+    : 'localhost'
+}:1741`;
 
 export { PROTOCOL, URL };
