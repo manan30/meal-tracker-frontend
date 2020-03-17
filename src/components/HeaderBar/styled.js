a5120be23c93fffdeeadec8e6eda8e29596c8a3e
Manan Joshi <mananjoshi1995@gmail.com>
2020-03-16 20:39:07 -0400

    Adjusting header to respond to mobile styles

--- src/components/HeaderBar/styled.js
+++ src/components/HeaderBar/styled.js
@@ -6,13 +6,18 @@ const Container = styled.div`
   padding: 20px;
   background-color: white;
   align-items: center;
+
+  @media screen and (min-width: 320px) and (max-width: 1024px) {
+    box-shadow: 0px 6px 20px rgba(13, 51, 32, 0.1);
+  }
 `;
 
 const IconsContainer = styled.div`
-  display: inline-flex;
+  display: flex;
   flex-basis: auto;
   flex-grow: 2;
   justify-content: space-evenly;
+  align-items: center;
   color: #363837;
 `;
 
