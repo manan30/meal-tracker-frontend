8fa9af422af509c479a9e8e9d090db81fc8b96e8
Manan Joshi <mananjoshi1995@gmail.com>
2020-04-15 17:56:27 -0400

    Refining modal component

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,8 +1,41 @@
 import React from 'react';
-import { ModalBackground } from './styled';
+import PropTypes from 'prop-types';
+import { MdClose } from 'react-icons/md';
+import {
+  ModalBackground,
+  ModalContainer,
+  ModalHeaderContainer,
+  ModalText,
+  CancelIcon,
+} from './styled';
 
-function Modal({ children, ...styles }) {
-  return <ModalBackground {...styles}>{children}</ModalBackground>;
+function Modal({ children, title, closeHandler }) {
+  return (
+    <ModalBackground>
+      <ModalContainer>
+        <ModalHeaderContainer>
+          <ModalText>{title}</ModalText>
+          <CancelIcon onClick={closeHandler}>
+            <MdClose />
+          </CancelIcon>
+        </ModalHeaderContainer>
+        {children}
+      </ModalContainer>
+    </ModalBackground>
+  );
 }
+
+Modal.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.elementType),
+    PropTypes.elementType,
+  ]).isRequired,
+  title: PropTypes.string,
+  closeHandler: PropTypes.func.isRequired,
+};
+
+Modal.defaultProps = {
+  title: '',
+};
 
 export default Modal;
--- src/components/Modal/styled.js
+++ src/components/Modal/styled.js
@@ -1,17 +1,69 @@
 import styled from 'styled-components';
+import Text from '../Text';
 
 const ModalBackground = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   position: absolute;
   left: 0;
   top: 0;
   height: 100%;
-  width: 100%;
+  width: 60%;
+
+  padding: 0 20%;
 
   background: rgba(40, 41, 40, 0.5);
   mix-blend-mode: normal;
   border-radius: 8px;
-
-  ${({ children, ...rest }) => rest}
 `;
 
-export { ModalBackground };
+const ModalContainer = styled.div`
+  padding: 25px;
+  width: calc(100% - 50px);
+
+  box-shadow: 0px 0px 5px rgba(13, 51, 32, 0.1);
+  border-radius: 8px;
+  background: white;
+
+  overflow: hidden;
+`;
+
+const ModalHeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  margin-bottom: 16px;
+`;
+
+const ModalText = styled(Text)`
+  font-size: 20px;
+  line-height: 24px;
+  color: #030f09;
+`;
+
+const CancelIcon = styled.div`
+  width: 24px;
+  height: 24px;
+  margin-left: auto;
+
+  color: #363837;
+
+  cursor: pointer;
+
+  svg {
+    height: 100%;
+    width: 100%;
+
+    opacity: 0.4;
+  }
+`;
+
+export {
+  ModalBackground,
+  ModalContainer,
+  ModalHeaderContainer,
+  ModalText,
+  CancelIcon,
+};
